24d8cc38cd83ee4eb713483ad1cac33940f774e0
Y.G.Lan <lany192@qq.com>
2015-06-22 23:26:25 +0800

    Initial commit

---
+++ app/src/main/java/com/lany/minesweeper/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.lany.minesweeper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        startActivity(new Intent(SplashActivity.this,GameActivity.class));
+        finish();
+    }
+
+}
