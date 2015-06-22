e789ed4f3d116d510f1acb2d7d829c5487f4771e
Y.G.Lan <lany192@qq.com>
2015-06-22 23:37:54 +0800

    update code

--- app/src/main/java/com/lany/minesweeper/SplashActivity.java
+++ app/src/main/java/com/lany/minesweeper/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.lany.minesweeper;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,9 +13,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postAtTime(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, GameActivity.class));
+                finish();
+            }
+        }, 2000);
 
-        startActivity(new Intent(SplashActivity.this,GameActivity.class));
-        finish();
     }
 
 }
